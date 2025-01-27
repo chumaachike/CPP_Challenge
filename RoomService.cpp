665fc39124cb2c2a04327c5f51461f82b7cee86d
chumaachike <edwardachike@gmail.com>
2025-01-27 19:00:49 +0000

    modify room service

--- RoomService.cpp
+++ RoomService.cpp
@@ -6,21 +6,31 @@ using namespace std;
 int main (){
 	cout << "Hello welcome to Frank's Carpet Cleaning Service" <<endl;
 	cout << endl;
-	cout <<"How many rooms would you like cleaned" <<endl;
-	int num_rooms {0};
-	cin >> num_rooms;
+	cout <<"How many small rooms would you like cleaned" <<endl;
+	int num_small_rooms {0};
+	cin >> num_small_rooms;
 	
-	const double price_per_room {32.5};
+	cout << "How many large rooms would you like cleaned" << endl;
+	int num_large_rooms {0};
+	cin >> num_large_rooms;
+	
+	const double price_per_small_room {25};
+	const double price_per_large_room {35};
 	const double sales_tax {0.06};
 	const int estimate_expiry {30}; // days
 	
+	double cost = (price_per_large_room * num_large_rooms) + (price_per_small_room * num_small_rooms);
+	
 	
 	cout << "\nEstimate for carpet cleaning service" << endl;
-    cout << "Number of rooms: " << num_rooms << endl;
-    cout << "Price per room: $" << price_per_room << endl;
-	cout << "Cost : $" << price_per_room * num_rooms << endl;
-	cout << "Tax: $" << price_per_room * num_rooms * sales_tax << endl;
-	cout << "Total estimate: $" << (price_per_room * num_rooms) + (price_per_room * num_rooms * sales_tax) << endl;
+    cout << "Number of  small rooms: " << num_small_rooms << endl;
+	cout << "Number of  large rooms: " << num_large_rooms << endl;
+    cout << "Price per small room: $" << price_per_small_room << endl;
+	cout << "Price per large room: $" << price_per_large_room << endl;
+	cout << "Cost : $" << cost << endl;
+	cout << "Tax: $" << cost * sales_tax << endl;
+	cout << "===============================" << endl;
+	cout << "Total estimate: $" << cost + (cost * sales_tax) << endl;
     cout << "This estimate is valid for " << estimate_expiry << " days" << endl;
 	
 	cout << endl;
